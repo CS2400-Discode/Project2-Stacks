dd240c32eadf55fd3c6ca5986a4f1337c2eb6522
shanecthomas <83098398+shanecthomas@users.noreply.github.com>
2022-03-20 15:27:56 -0700

    Added Infix Tests

--- UnitTests/CalculatorTest.java
+++ UnitTests/CalculatorTest.java
@@ -1,5 +1,38 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+import org.junit.jupiter.api.Test;
 
+/***
+ * Uses JUnit to test different scenarios for the convertToPostFix and evaluatePostfix methods in Calc.java
+ */
+class CalculatorTest {
+    /**
+     * Tests to see if the infix expression being empty affects the postfix expression.
+     */
+    @Test
+    void nullInfix() {
+        String exp = "";
+        String postFix = Calc.convertToPostFix(exp);
+        assertEquals("", postFix);
+    }
+
+    /**
+     * Tests to see if the infix expression having no operands affects the postfix expression.
+     */
+    @Test
+    void nullOperandInfix() {
+        String exp = "abcdefg";
+        String postFix = Calc.convertToPostFix(exp);
+        assertEquals("abcdefg", postFix);
+    }
+
+    /**
+     * Tests to see if the infix expression having no variables affects the postfix expression.
+     */
+    @Test
+    void nullVariableInfix() {
+        String exp = "*/+-";
+        String postFix = Calc.convertToPostFix(exp);
+        assertEquals("*/+-", postFix);
+    }
 }
